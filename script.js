90a2fcfe18fed1252e40aa603e4af7b4608e7be2
thebigdavec <daverbcollison@gmail.com>
2021-04-11 14:11:24 +0100

    Add logic and styling for progress bar.

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ const btnPlay = document.querySelector('.play')
 const btnPlayIcon = btnPlay.querySelector('i')
 const player = document.getElementById('player')
 const song = document.querySelector('.song')
+const progress = document.querySelector('.progress')
 
 btnPlay.addEventListener('click', () => {
   player.classList.toggle('playing')
   if (player.classList.contains('playing')) {
     btnPlayIcon.classList.remove('fa-pause')
     btnPlayIcon.classList.add('fa-play')
+    song.addEventListener('timeupdate', () => {
+      const duration = song.duration
+      const currentTime = song.currentTime
+      const position = (currentTime / duration) * 100
+
+      progress.value = position
+    })
     song.play()
   } else {
     btnPlayIcon.classList.remove('fa-play')
     btnPlayIcon.classList.add('fa-pause')
     song.pause()
+    song.removeEventListener('timeupdate')
   }
 })
