879ce08adfda1e128a25d1302aabeaa399bbdcef
thebigdavec <daverbcollison@gmail.com>
2021-04-11 15:31:45 +0100

    Favicon and progress bar
    Add favicon png
    add colour changes to progress bar.
    add song reset at end of track.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnPlay = document.querySelector('.play')
 const btnPlayIcon = btnPlay.querySelector('i')
+const btnNext = document.querySelector('.next')
 const player = document.getElementById('player')
 const song = document.querySelector('.song')
 const progress = document.querySelector('.progress')
@@ -14,13 +15,35 @@ btnPlay.addEventListener('click', () => {
       const currentTime = song.currentTime
       const position = (currentTime / duration) * 100
 
-      progress.value = position
+      if (position <= 100) progress.value = position
+      if (position > 95) {
+        progress.classList.add('finishing')
+        if (position >= 100) {
+          stopSong()
+        }
+      } else {
+        progress.classList.remove('finishing')
+      }
     })
     song.play()
   } else {
     btnPlayIcon.classList.remove('fa-play')
     btnPlayIcon.classList.add('fa-pause')
     song.pause()
-    song.removeEventListener('timeupdate')
+    song.removeEventListener('timeupdate', () => {
+      console.log('Song paused')
+    })
   }
 })
+
+btnNext.addEventListener('click', () => {
+  song.currentTime = (song.duration / 10) * 9.4
+})
+
+function stopSong() {
+  player.classList.remove('playing')
+  btnPlayIcon.classList.remove('fa-pause')
+  btnPlayIcon.classList.add('fa-play')
+  progress.value = 0
+  song.load()
+}
